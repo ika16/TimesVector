cd917637bb192f94effebeec187dbbf6c89ad1f6
ika16 <60054524+ika16@users.noreply.github.com>
2020-11-09 18:26:41 +0700

    Update classify_genes.py

--- bin/classify_genes.py
+++ bin/classify_genes.py
@@ -16,7 +16,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-$sudo-apt get upgrade --fix-missing
+$ sudo-apt get upgrade --fix-missing
 
 import sys
 import math
